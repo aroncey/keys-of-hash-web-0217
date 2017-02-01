5c554436f64fe29ae21f2ac8f3ec7009adbdf0fe
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-01-31 19:10:45 -0500

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+	def keys_of(*arguments)
+		array_arguments = arguments.collect {|keys| keys}
+		hash_results = Hash.new
+		i = 0
+			while i < array_arguments.length do
+				hash_results.merge!(self.select {|keys,value| value == array_arguments[i]})
+				i += 1
+			end
+		hash_results.keys
+	end
 end
